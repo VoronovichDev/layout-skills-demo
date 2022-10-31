dc74c5309e69cf80b6b710bc5314b9f43a44efcc
Voronovich_dev <alexanderbizv1@gmail.com>
2022-11-01 01:23:30 +0300

    feat: create company, auctions and how-it-works blocks, set up swiper for slider in auctions

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,17 @@ import {
 import AOS from 'aos';
 
 flsFunctions.isWebp();
+
+const swiper = new Swiper('.auctions-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next1',
+    prevEl: '.swiper-button-prev1',
+  },
+  modules: [Navigation, FreeMode],
+  autoHeight: true,
+  speed: 500,
+  slidesPerView: 3,
+  spaceBetween: 42,
+  freeMode: true,
+  grabCursor: true,
+});
