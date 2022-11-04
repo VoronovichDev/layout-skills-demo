0eff82499d019e335546884af8247a58ac6561fa
Voronovich_dev <alexanderbizv1@gmail.com>
2022-11-04 23:46:58 +0300

    finish v1.0.0

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -12,11 +12,13 @@ burgerClose.addEventListener('click', (e) => {
   toggleBurger();
 });
 
-headerLink.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    toggleBurger();
+if (window.innerWidth <= 700) {
+  headerLink.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      toggleBurger();
+    });
   });
-});
+}
 
 function toggleBurger() {
   burgerItem.classList.toggle('active');
