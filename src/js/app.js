14b94444f13d61874110678009f654038aa7a684
Voronovich_dev <alexanderbizv1@gmail.com>
2022-11-04 22:03:02 +0300

    feat: finish adaptive layout

--- src/js/app.js
+++ src/js/app.js
@@ -15,47 +15,102 @@ import AOS from 'aos';
 
 flsFunctions.isWebp();
 
-const swiper = new Swiper('.auctions-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next1',
-    prevEl: '.swiper-button-prev1',
-  },
-  modules: [Navigation, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 3,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+const swiper = () => {
+  new Swiper('.auctions-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+    modules: [Navigation, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
 
-const swiper2 = new Swiper('.popular-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next2',
-    prevEl: '.swiper-button-prev2',
-  },
-  modules: [Navigation, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 2.5,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+const swiper2 = () => {
+  new Swiper('.popular-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+    modules: [Navigation, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 2.5,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 2.5,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
 
-const swiper3 = new Swiper('.categories-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next3',
-    prevEl: '.swiper-button-prev3',
-  },
-  modules: [Navigation, FreeMode],
-  autoHeight: true,
-  speed: 500,
-  slidesPerView: 3,
-  spaceBetween: 42,
-  freeMode: true,
-  grabCursor: true,
-});
+const swiper3 = () => {
+  new Swiper('.categories-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next3',
+      prevEl: '.swiper-button-prev3',
+    },
+    modules: [Navigation, FreeMode],
+    autoHeight: true,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 15,
+    freeMode: true,
+    grabCursor: true,
+    breakpoints: {
+      1300: {
+        spaceBetween: 42,
+      },
+      1100: {
+        slidesPerView: 3,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  });
+};
+
+function detectDevice() {
+  if (window.innerWidth < 600) {
+    for (let i = 0; i <= 2; i++) {
+      document.querySelectorAll('.auctions-slide')[i].style.display = 'block';
+      document.querySelectorAll('.popular-slide')[i].style.display = 'block';
+      document.querySelectorAll('.categories-slide')[i].style.display = 'block';
+    }
+  } else {
+    swiper();
+    swiper2();
+    swiper3();
+  }
+}
+
+detectDevice();
 
 AOS.init({
   // Global settings:
